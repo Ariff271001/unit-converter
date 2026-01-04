48855aa449685a085e9947b1abcd634a7632d6e7
Ariff Haikal <76397403+Ariff271001@users.noreply.github.com>
2026-01-04 21:32:24 +0800

    Completed the app functionality
    1.Generate all conversions when user click "Convert"
    2.Round the numbers down to three decimal places.
    3.Alert user when input incorrect number format.
    4.Included app design

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const input = document.getElementById("input");
+const btn = document.getElementById("btn");
+const length = document.getElementById("length");
+const volume = document.getElementById("volume");
+const mass = document.getElementById("mass");
+
+btn.addEventListener("click", function () {
+  const inputValue = input.value;
+  if (inputValue) {
+    const meterToFeet = (inputValue * 3.281).toFixed(3);
+    const feetToMeter = (inputValue / 3.281).toFixed(3);
+    const litersToGallons = (inputValue * 0.264).toFixed(3);
+    const gallonsToLiters = (inputValue / 0.264).toFixed(3);
+    const kilosToPounds = (inputValue * 2.204).toFixed(3);
+    const poundsToKilos = (inputValue / 2.204).toFixed(3);
+
+    length.textContent = `${inputValue} meters = ${meterToFeet} | ${inputValue} feet = ${feetToMeter} meters`;
+    volume.textContent = `${inputValue} meters = ${litersToGallons} | ${inputValue} feet = ${gallonsToLiters} meters`;
+    mass.textContent = `${inputValue} meters = ${kilosToPounds} | ${inputValue} feet = ${poundsToKilos} meters`;
+  } else {
+    const alertMessage = "Please insert the correct number to convert";
+    alert(alertMessage);
+    length.textContent = alertMessage;
+    volume.textContent = alertMessage;
+    mass.textContent = alertMessage;
+  }
+});
